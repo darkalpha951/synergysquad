914b9b82be2614114aecc4b17a7d0157ac1be6d7
Garvit Singh <garvitsingh006@gmail.com>
2025-05-22 10:59:31 +0530

    some minor changes

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -3,7 +3,6 @@ import ProblemSection from './ProblemSection';
 import SolutionSection from './SolutionSection';
 import FeaturesSection from './FeaturesSection';
 import AnalysisSection from './AnalysisSection';
-import TeamSection from './TeamSection';
 import Footer from './Footer';
 
 function Home() {
